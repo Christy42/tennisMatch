0ae31c18cc74b4061e581e7c6e36236bdf280734
Mark <mark.christiansen@nuim.ie>
2017-08-06 00:37:04 +0100

    Create basic file to set ranking points

--- player/rankings.py
+++ player/rankings.py
@@ -1,7 +1,11 @@
 import yaml
 import os
+import datetime
+import random
 
 
+# tie breaks, 1000 + events score, highest score, second highest score etc, random 1, random 2
+# I need format to be competitions = {date: {event level: level, ranking points: score, event name: name, court: court}}
 def update_rankings():
     rankings = {}
     for file in os.listdir("players//players"):
@@ -9,6 +13,66 @@ def update_rankings():
             player = yaml.safe_load(player_file)
             tournaments = player["rankings"]
             name = player["name"]
+            id = player["id"]
         rankings.update({file[7:]: {"ranking points": sum(list(tournaments.values())), "name": name}})
     with open("players//rankings.yaml", "w") as ranking_file:
         yaml.safe_dump(rankings, ranking_file)
+
+
+def update_player_rankings(last_year):
+    # format it as ranking points = {senior: {point: , tie breakers: {1000: 1:,...., random_one, random_two}}}
+    for file in os.listdir(os.environ["TENNIS_HOME"] + "//players//players"):
+        with open(os.environ["TENNIS_HOME"] + "//players//players//" + file, "r") as player_file:
+            player = yaml.safe_load(player_file)
+        # TODO: Need to calculate Junior rankings, Wimbledon rankings, senior rankings and doubles rankings
+        mandatory = player["mandatory"]
+        competitions = player["tournaments"]
+        junior = get_ranking_points("junior", 6, 1000, competitions, last_year)
+        junior_doubles = get_ranking_points("junior doubles", 6, 1000, competitions, last_year)
+        doubles = get_ranking_points("junior doubles", 14, 1000, competitions, last_year)
+        if junior is not None:
+            junior = {"points": junior["points"] + junior_doubles["points"] / 4.0,
+                      "tie breakers": junior["tie breakers"]}
+        if not mandatory:
+            senior = get_ranking_points("singles", 18, 1000, competitions, last_year)
+        else:
+            pass
+        # TODO: If mandatory
+        # TODO: Get each masters, Slam, WTF ranking and all 500s, 250s and go from there?
+        competitions = {date_comp: competitions[date_comp] for date_comp in competitions
+                        if competitions[date_comp]["court"] == "grass"}
+        wimbledon_two_year = get_ranking_points("singles", 9999, 1000, competitions,
+                                                last_year - datetime.timedelta(days=365))
+        wimbledon_one_year = get_ranking_points("singles", 9999, 1000, competitions,
+                                                last_year)
+        wimbledon = {"points": senior["points"] + 0.75 * wimbledon_two_year["points"] +
+                     0.25 * wimbledon_one_year["points"], "tie breakers": wimbledon_two_year["tie breakers"]}
+        player["ranking_points"] = {"senior": senior, "doubles": doubles, "junior": junior, "wimbledon": wimbledon}
+        # TODO: Remove useless tie breaks
+        print(senior)
+        print(doubles)
+        print(junior)
+        print(wimbledon)
+        with open(os.environ["TENNIS_HOME"] + "//players//players//" + file, "w") as player_file:
+            yaml.safe_dump(player, player_file)
+
+def get_ranking_points(style, number, tie_break_level, competitions, last_year):
+    main_tie_break = 0
+    tie_breakers = {i: {"level": 0, "score": 0, "name": "default"} for i in range(1, number + 1)}
+    senior_comps = {date_comp: competitions[date_comp] for date_comp in competitions
+                    if date_comp > last_year and competitions[date_comp]["style"] == style}
+    for competition in senior_comps:
+        if senior_comps[competition]["level"] >= tie_break_level:
+            main_tie_break += senior_comps[competition]["score"]
+        for tier in tie_breakers:
+            if tie_breakers[tier]["score"] < senior_comps[competition]["score"]:
+                tie_breakers = {level: tie_breakers[level - 1 * (level > tier)] for level in tie_breakers}
+                tie_breakers[tier] = {"score": senior_comps[competition]["score"],
+                                      "name": senior_comps[competition]["name"],
+                                      "level": senior_comps[competition]["level"]}
+                break
+    ranking_points = sum([tie_breakers[score]["score"] for score in tie_breakers])
+    tie_breakers.update({"random 1": random.random(), "random 2": random.random(), "ATP Score": main_tie_break})
+    result = {"points": ranking_points, "tie breakers": tie_breakers}
+    return result
+update_player_rankings(datetime.date(2000, 7, 15))
